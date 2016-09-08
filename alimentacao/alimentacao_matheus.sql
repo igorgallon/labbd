6617b03ea64f06d5fd373245e9af8eda8adeb8de
matheusDeAlmeida <mat.sts.amd@gmail.com>
2016-09-08 17:23:40 -0300

    Create alimentacao_matheus.sql

---
+++ alimentacao/alimentacao_matheus.sql
@@ -0,0 +1,14 @@
+insert into disciplina(codigoDaDisciplina, creditosPraticos, creditosTeoricos, siglaDepartamento)
+values
+(020338, 0, 4, 'DC'), --intro a logica
+(089109, 0, 4, 'DM'), --calculo i
+(099015, 0, 4, 'DF'), --fisica i
+(071170, 0, 4, 'DQ'); --fundamentos de quimica
+
+
+insert into departamento(sigla, centroAcademico, nome)
+values
+('DC', 'CCET', 'Departamento de Computacao'),
+('DM', 'CCET', 'Departamento de Matemática'),
+('DF', 'CCET', 'Departamento de Física'),
+('DQ', 'CCET', 'Departamento de Química');
