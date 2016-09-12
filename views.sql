13aacc60cac706d370e0abb7a86b22c6f5e310eb
Igor Felipe Gallon <igor.felipe.gallon@outlook.com>
2016-09-12 20:13:05 -0300

    Criação de procedure e adição de uma view

--- views.sql
+++ views.sql
@@ -1,15 +1,15 @@
 
 --Materia visão do aluno:
 --idturma-nomedisciplina-horainicial-horafinal(calculada)-diadasemana-letra-vagas
-
+-- ESSA É DO GUILHEME
 CREATE OR REPLACE VIEW vDisciplinasEmOferta AS
 	SELECT tu.idturma, d.nome, th.horario, (th.horario + th.duracao*3600) AS horariofim, th.diaDasemana, tu.letra, tu.vagas
 	FROM turma AS tu, disciplina AS d, turmahorario AS th
 	WHERE tu.idturma = th.idturma AND tu.coddisciplina = d.codigodisciplina;  
 	
---Plano de Ensino (visão do aluno):
---nomedisciplina-nomeprofessor-situacao-letra-ano-semestre-vagas
-
+-- Plano de Ensino (visão do aluno):
+-- nomedisciplina-nomeprofessor-situacao-letra-ano-semestre-vagas
+-- ESSA É DO PÃO
 CREATE OR REPLACE VIEW vPlanoDeEnsino AS
 	SELECT d.nome AS Disciplina,tu.letra, tu.ano, tu.semestre, tu.vagas, pde.estado, 
 	pe.nome AS Professor FROM disciplina AS d, planodeensino AS pde, turma AS tu, 
@@ -17,9 +17,9 @@ CREATE OR REPLACE VIEW vPlanoDeEnsino AS
 	WHERE tu.coddisciplina = d.codigodisciplina AND tu.idturma = pde.idturma 
 		AND doc.SIAPE = pde.SIAPE AND doc.SIAPE = se.SIAPE AND se.CPF = pe.CPF; 
 	
---Docentes com cargo administrativo atualmente:
---SIAPE-nomedocente-departamento-posicao-tipodocente-datainicio-cargoadministrativocorrente
-
+-- Docentes com cargo administrativo atualmente:
+-- SIAPE-nomedocente-departamento-posicao-tipodocente-datainicio-cargoadministrativocorrente
+-- ESSA É DO TUTUI
 CREATE OR REPLACE VIEW vDocenteCargoAdministrativo AS
 	SELECT d.siape, pe.nome AS Docente, de.nome AS Departamento, dca.periodo_inicio AS iniciodomandato,
 	 d.tipo_docente, dca.nome_cargo
@@ -32,8 +32,7 @@ CREATE OR REPLACE VIEW vDocenteCargoAdministrativo AS
 	
 --Membros que participaram de um conselho:
 --nome-siape/RA-portariadeindicacao-telefone-docente/discente/TA-representacao
-
-	 	 
+-- ESSA É DO MATHEUS
 -- siape-nome-telefone-cpf-email-endereco (servidor+pessoa)
 CREATE OR REPLACE VIEW vContatosServidor AS
 	SELECT s.siape, pe.nome, pe.cpf, pe.endereco_cidade, pe.endereco_bairro,
@@ -42,8 +41,9 @@ CREATE OR REPLACE VIEW vContatosServidor AS
 	FROM SERVIDOR AS s, PESSOA AS PE
 	WHERE s.cpf = pe.cpf;
 	
---Membros que participaram de um conselho:
---nome-siape/RA-portariadeindicacao-telefone-docente/discente/TA-representacao
+-- Membros que participaram de um conselho:
+-- nome-siape/RA-portariadeindicacao-telefone-docente/discente/TA-representacao
+-- ESSA É DO VASSOURA
 /*
 CREATE OR REPLACE VIEW vHistoricoConselho AS
 	SELECT pe.nome AS Nome, s.siape AS Identificacao, mem.nro_portaria_indicacao AS Nro_Portaria,
@@ -56,3 +56,13 @@ CREATE OR REPLACE VIEW vHistoricoConselho AS
 	FROM Pessoa AS pe, Discente AS di, Membro AS mem
 	WHERE pe.cpf = mem.cpf AND mem.cpf = di.cpf;
 */
+
+CREATE OR REPLACE VIEW vHistoricoPlanoEnsino AS
+	SELECT d.nome AS Disciplina,tu.letra, tu.ano, tu.semestre, tu.vagas, pde.estado, 
+	pe.nome AS Professor, r.dataDeRevisao AS Data_Da_Revisao, r.parecer AS Parecer,
+	r.pe_estado AS Estado_Do_Plano_Ensino, r.r_estado AS Estado_Da_Revisao
+	FROM disciplina AS d, planodeensino AS pde, turma AS
+	tu, docente AS doc, servidor AS se, pessoa AS pe, revisao AS r
+	WHERE tu.coddisciplina = d.codigodisciplina AND tu.idturma = pde.idturma 
+		AND doc.SIAPE = pde.SIAPE AND doc.SIAPE = se.SIAPE AND se.CPF = pe.CPF AND
+		doc.SIAPE = r.SIAPE AND pde.idturma = r.idturma;
