e998c1c08513a9c5d5c703ace0e1f1d70b245355
Igor Felipe Gallon <igor.felipe.gallon@outlook.com>
2016-09-12 12:20:31 -0300

    Criação da quinta view

--- views.sql
+++ views.sql
@@ -3,7 +3,7 @@
 --idturma-nomedisciplina-horainicial-horafinal(calculada)-diadasemana-letra-vagas
 
 CREATE OR REPLACE VIEW vDisciplinasEmOferta AS
-	SELECT tu.idturma, d.nome, th.horario, (th.horario + th.duracao) AS horariofim, th.diaDasemana, tu.letra, tu.vagas
+	SELECT tu.idturma, d.nome, th.horario, (th.horario + th.duracao*3600) AS horariofim, th.diaDasemana, tu.letra, tu.vagas
 	FROM turma AS tu, disciplina AS d, turmahorario AS th
 	WHERE tu.idturma = th.idturma AND tu.coddisciplina = d.codigodisciplina;  
 	
@@ -37,9 +37,22 @@ CREATE OR REPLACE VIEW vDocenteCargoAdministrativo AS
 -- siape-nome-telefone-cpf-email-endereco (servidor+pessoa)
 CREATE OR REPLACE VIEW vContatosServidor AS
 	SELECT s.siape, pe.nome, pe.cpf, pe.endereco_cidade, pe.endereco_bairro,
-	pe.endereco_numero, pe.endereco_logradouro, pe.endereco_complemento, pe.endereco_cep
+	pe.endereco_numero, pe.endereco_logradouro, pe.endereco_complemento, pe.endereco_cep,
+	pe.email1, pe.email2, pe.telefone1, pe.telefone2, pe.telefone3
 	FROM SERVIDOR AS s, PESSOA AS PE
 	WHERE s.cpf = pe.cpf;
 	
 --Membros que participaram de um conselho:
 --nome-siape/RA-portariadeindicacao-telefone-docente/discente/TA-representacao
+/*
+CREATE OR REPLACE VIEW vHistoricoConselho AS
+	SELECT pe.nome AS Nome, s.siape AS Identificacao, mem.nro_portaria_indicacao AS Nro_Portaria,
+	pe.telefone1 AS Telefone_1, pe.telefone2 AS Telefone_2, mem.representacao AS Representacao
+	FROM Pessoa AS pe, Servidor AS s, Membro AS mem
+	WHERE pe.cpf = mem.cpf AND mem.cpf = s.cpf
+	UNION
+	SELECT pe.nome AS Nome, di.ra AS Identificacao, mem.nro_portaria_indicacao AS Nro_Portaria,
+	pe.telefone1 AS Telefone_1, pe.telefone2 AS Telefone_2, mem.representacao AS Representacao
+	FROM Pessoa AS pe, Discente AS di, Membro AS mem
+	WHERE pe.cpf = mem.cpf AND mem.cpf = di.cpf;
+*/
