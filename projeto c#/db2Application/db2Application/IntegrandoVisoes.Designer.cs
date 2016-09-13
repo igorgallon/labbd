d1970a075a814537c9ba976ee60b20cf96a8cb88
Guilherme Ramos <gmatheus95@outlook.com>
2016-09-13 17:20:12 -0300

    Organização do Git + ferramenta C#

---
+++ projeto c#/db2Application/db2Application/IntegrandoVisoes.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace db2Application
+{
+    partial class IntegrandoVisoes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnExecutaProc = new System.Windows.Forms.Button();
+            this.cmb_tabelas = new System.Windows.Forms.ComboBox();
+            this.cmb_views = new System.Windows.Forms.ComboBox();
+            this.cmb_procedures = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(55, 95);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(638, 324);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick_1);
+            // 
+            // btnExecutaProc
+            // 
+            this.btnExecutaProc.Location = new System.Drawing.Point(229, 471);
+            this.btnExecutaProc.Name = "btnExecutaProc";
+            this.btnExecutaProc.Size = new System.Drawing.Size(133, 23);
+            this.btnExecutaProc.TabIndex = 1;
+            this.btnExecutaProc.Text = "button1";
+            this.btnExecutaProc.UseVisualStyleBackColor = true;
+            this.btnExecutaProc.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // cmb_tabelas
+            // 
+            this.cmb_tabelas.FormattingEnabled = true;
+            this.cmb_tabelas.Location = new System.Drawing.Point(53, 55);
+            this.cmb_tabelas.Name = "cmb_tabelas";
+            this.cmb_tabelas.Size = new System.Drawing.Size(224, 21);
+            this.cmb_tabelas.TabIndex = 3;
+            this.cmb_tabelas.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // cmb_views
+            // 
+            this.cmb_views.FormattingEnabled = true;
+            this.cmb_views.Location = new System.Drawing.Point(501, 55);
+            this.cmb_views.Name = "cmb_views";
+            this.cmb_views.Size = new System.Drawing.Size(190, 21);
+            this.cmb_views.TabIndex = 4;
+            this.cmb_views.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // cmb_procedures
+            // 
+            this.cmb_procedures.FormattingEnabled = true;
+            this.cmb_procedures.Location = new System.Drawing.Point(53, 473);
+            this.cmb_procedures.Name = "cmb_procedures";
+            this.cmb_procedures.Size = new System.Drawing.Size(170, 21);
+            this.cmb_procedures.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(50, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Tabelas";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(498, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Views";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(53, 457);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Procedures";
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(51, 9);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(149, 25);
+            this.lblTitulo.TabIndex = 11;
+            this.lblTitulo.Text = "VISÃO ATUAL";
+            this.lblTitulo.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // IntegrandoVisoes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(762, 520);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmb_procedures);
+            this.Controls.Add(this.cmb_views);
+            this.Controls.Add(this.cmb_tabelas);
+            this.Controls.Add(this.btnExecutaProc);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "IntegrandoVisoes";
+            this.Text = "Demonstrativo do BD";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnExecutaProc;
+        private System.Windows.Forms.ComboBox cmb_tabelas;
+        private System.Windows.Forms.ComboBox cmb_views;
+        private System.Windows.Forms.ComboBox cmb_procedures;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblTitulo;
+    }
+}
+
---
+++ projeto c#/db2Application/db2Application/IntegrandoVisoes.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using IBM.Data.DB2;
+using Microsoft.VisualBasic;
+
+
+namespace db2Application
+{
+    public partial class IntegrandoVisoes : Form
+    {
+        public IntegrandoVisoes()
+        {
+            InitializeComponent();
+        }
+
+        string[][] matrizParams;
+
+
+        DB2Connection _con;
+        DataTable Table;
+
+        private void connection()
+        {
+            #region senha
+            _con = new DB2Connection("User ID=db2admin;Password=123;Server=localhost;Database=DIGRA;Current Schema=DB2ADMIN");
+            _con.Open();
+            #endregion
+
+
+        }
+
+        private void tablesandviews(string selecionado)
+        {
+            try
+            {
+                Table = new DataTable("TestTable");
+                using (DB2Command _cmd = new DB2Command("SELECT * FROM " + selecionado, _con))
+                {
+                    DB2DataAdapter _dap = new DB2DataAdapter(_cmd);
+                    _dap.Fill(Table);
+                    dataGridView1.DataSource = Table;
+                }
+            }
+            catch { }
+        }
+
+        private void executeProcedure(string procname, string[] parametros)
+        {
+            try
+            {
+                string parametrosParsing = "";
+                foreach (string singleparam in parametros)
+                {
+                    parametrosParsing += '\'' + singleparam + '\'' + ", ";
+                }
+                parametrosParsing = parametrosParsing.Substring(0, parametrosParsing.Length - 2);
+                using (DB2Command _cmd = new DB2Command("CALL " + procname + "(" + parametrosParsing + ")", _con))
+                {
+                    _cmd.ExecuteNonQuery();
+                    MessageBox.Show("Comando executado!");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Comando não executado!");
+            }
+        }
+
+        private void closeconnection()
+        {
+            _con.Close();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] vetortabelas = new string[36]{"TURMA","CONSELHO","CONSELHODEDEPARTAMENTO","CURSO","DEPARTAMENTO","DISCIPLINA","DOCENTE","DOCENTE_CARGO_ADMINISTRATIVO",
+                "MEMBRO","PESSOA","PLANODEENSINO","REQUISITOS","REVISAO","SERVIDOR","TAS","TURMA","TURMAHORARIO","ATIVIDADE","CALENDARIO","CONSELHO","CONSELHODEDEPARTAMENTO",
+                "DISCENTE","DISCENTE_CURSOU_TURMA","DISCENTE_EAD","DISCENTE_INCREVE_TURMA","DISCENTE_PRESENCIAL","ENADE","ENADE_AVALIA_CURSO","ITEM","ITEM_DE_PAUTA","PROPOE",
+                "RECESSO","RECONHECIMENTO","REQUISITOS","REUNIAO","VISITA"};
+            string[] vetorviews = new string[13]{"CALENDARIOVIEW","REUNIAO_COM_PAUTAS","V_APROVACOES_POR_MEMBRO","V_PROPOSICOES_DO_ANO_CORRENTE",
+            "V_PROPOSICOES_DO_MES_CORRENTE","VATIVIDADE","VCONTATOSERVIDOR","VDISCIPLINASEMOFERTA","VDOCENTECARGOADMINISTRATIVO","VHISTORICOPLANOENSINO","VPLANODEENSINO",
+            "VRECESSO","VTURMAHORARIOINCONSISTENTE"};
+            string[] vetorprocedures = new string[5] { "ADICIONAMEMBROCONSELHO", "ALTERAESTADOPLANODEENSINO", "NOVOCONSELHO", "PINSERESERVIDOR", "PREMOVEDISCIPLINA" };
+            string[] vetorfunctions = new string[5] { "APROVACAO_PLANO_ENSINO_DOCENTE", "TIPO_DOCENTES_PORCENTAGEM", "MEDIA_CREDITOS_PRATICOS_DISCIPLINA", "QUANTIDADE_DISCIPLINA_DEPARTAMENTO", "DISCIPLINA_TEM_QUANTOS_REQUISITOS" };
+
+
+            matrizParams = new string[10][]; //10 procedures e functions
+            //PARAMETROS DAS PROCEDURES
+            matrizParams[0] = new string[3] { "VAL_SIAPE", "VAL_CONSELHO", "VAL_PORTARIA" }; //nome da funcao + parametros
+            matrizParams[1] = new string[7] { "SIAPE_P", "NOVO_ESTADO", "NOME_DISCIPLINA", "LETRA", "ANO", "SEMESTRE", "PARECER" };
+            matrizParams[2] = new string[2] { "VAL_TIPO", "SIGLA" };
+            matrizParams[3] = new string[6] { "SIAPE","NOME","POSICAO","TIPO_DOCENTE","CPF","TIPO_SERVIDOR" };
+            matrizParams[4] = new string[1] { "CODIGODADISCIPLINA" };
+            //PARAMETROS DAS FUNCTIONS
+            matrizParams[5] = new string[1] { "PAR_NOME" }; //nome da funcao + parametros
+            matrizParams[6] = new string[1] { "VAL_TIPO_DOCENTE" };
+            matrizParams[7] = null;
+            matrizParams[8] = new string[1] { "SEI LA" };
+            matrizParams[9] = new string[1] { "procedure que falta" };
+
+
+            cmb_tabelas.Items.AddRange(vetortabelas);
+            cmb_views.Items.AddRange(vetorviews);
+            cmb_procedures.Items.AddRange(vetorprocedures);
+            connection();
+
+            btnExecutaProc.Text = "Executa Procedure";
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cmb_views.Text = "";
+            tablesandviews(cmb_tabelas.Text);
+            lblTitulo.Text = "Tabela " + cmb_tabelas.Text;
+            
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cmb_tabelas.Text = "";
+            tablesandviews(cmb_views.Text);
+            lblTitulo.Text = "View " + cmb_views.Text;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string mensagemUsuario = "Insira os parametros necessários separados por ponto e virgula\n";
+            foreach (string parametro in matrizParams[cmb_procedures.SelectedIndex])
+            {
+                mensagemUsuario += parametro + "\n";
+            }
+            string input = Interaction.InputBox(mensagemUsuario, cmb_procedures.Text, "Digite sua entrada aqui", -1, -1);
+            string[] parametros = input.Split(';');
+            executeProcedure(cmb_procedures.Text, parametros);
+        }
+
+        private void dataGridView1_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow item in this.dataGridView1.SelectedRows)
+            {
+                
+                try
+                {
+                    using (DB2Command _cmd = new DB2Command("DELETE FROM VDOCENTECARGOADMINISTRATIVO WHERE SIAPE = \'" + item.Cells[0].Value.ToString() + 
+                        "\' AND INICIODOMANDATO = \'" + Convert.ToDateTime(item.Cells[3].Value.ToString()).ToString("yyyy-MM-dd") + '\''))
+                        //'11/1/2015 12:00:00 AM'
+                    {
+                        _cmd.ExecuteNonQuery();
+                        MessageBox.Show("Comando executado!");
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("Comando não executado!");
+                }
+            }
+        }
+    }
+}
