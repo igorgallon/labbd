70abd3682bc945c8e70a9da6df99b4bf774c561e
Guilherme <gmatheus95@outlook.com>
2016-09-12 22:07:41 -0300

    .

--- procedures/guilherme_tutui.sql
+++ procedures/guilherme_tutui.sql
@@ -1,15 +1,15 @@
-CREATE OR REPLACE VIEW vTurmaHorarioInconsistente AS
-	SELECT th.idturma, tu.coddisciplina 
-	FROM turmahorario AS th, turma AS tu, disciplina AS d
-	WHERE th.idturma = tu.idturma AND tu.coddisciplina = d.codigodisciplina
-	AND (d.creditosteoricos+d.creditospraticos) NOT IN 
-		( 
-		SELECT SUM(tho.duracao) FROM turmahorario 
-		AS tho, turma AS tur, disciplina AS di 
-		WHERE tho.idturma = tur.idturma AND tur.coddisciplina = di.codigodisciplina
-		AND tho.idturma = th.idturma
-		)
-	
-
-/*
-		*/
+CREATE OR REPLACE TRIGGER t_vContatosServidor
+INSTEAD OF UPDATE
+ON vContatosServidor
+REFERENCING NEW AS novo OLD AS velho
+FOR EACH ROW
+BEGIN ATOMIC
+	VALUES(CASE WHEN novo.siape = velho.siape AND novo.CPF = velho.CPF THEN 0 
+                ELSE RAISE_ERROR('70002', 'Must not change SIAPE or CPF') END);
+    UPDATE pessoa AS pe
+      SET (nome, endereco_cidade, endereco_bairro, endereco_numero, endereco_logradouro,
+      		endereco_cep, email1, email2, telefone1, telefone2, telefone3)
+        = (novo.nome, novo.endereco_cidade, novo.endereco_bairro, novo.endereco_numero, novo.endereco_logradouro,
+      		novo.endereco_cep, novo.email1, novo.email2, novo.telefone1, novo.telefone2, novo.telefone3)
+    WHERE novo.CPF = pe.CPF;
+END}
