3b5f873ad744758e717dc451c38d0d047e6c1c3e
Victor Otavio <victor_otavio205@hotmail.com>
2016-09-13 02:01:35 -0300

    Delete testeViews.sql

--- testeViews.sql
+++
@@ -1,6 +0,0 @@
---SELECT * FROM membro
---SELECT * FROM PESSOA
---SELECT * FROM vDisciplinasEmOferta OK
-SELECT * FROM vdocentecargoadministrativo OK
---SELECT * FROM VCONTATOSSERVIDOR ok
---SELECT * FROM VPLANODEENSINO ok
